7ebdd16ecca41884f4890c3453527865ab8a6e3c
Lalit Patil <patilld94@gmail.com>
2026-03-14 16:38:07 +0530

    Hello world

---
+++ src/main/java/com/dev/planmytrip/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.dev.planmytrip.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> hello() {
+
+        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+    }
+}
