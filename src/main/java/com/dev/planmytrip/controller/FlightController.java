3b5d91c0ec5a877fc42d14aa823f79d9fcce34ca
Lalit Patil <patilld94@gmail.com>
2026-03-22 16:12:25 +0530

    FLightController flights

--- src/main/java/com/dev/planmytrip/controller/FlightController.java
+++ src/main/java/com/dev/planmytrip/controller/FlightController.java
@@ -1,9 +1,18 @@
 package com.dev.planmytrip.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FlightController {
+
+    @GetMapping("/filght")
+    public ResponseEntity<?> getFlight() {
+
+        return new ResponseEntity<>("Flights", HttpStatus.OK);
+    }
 
 
 }
