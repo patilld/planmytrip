e047607e25296e3a01b822493d0a3b96669184ce
Lalit Patil <patilld94@gmail.com>
2026-03-22 16:37:34 +0530

    FLightController flights save

--- src/main/java/com/dev/planmytrip/controller/FlightController.java
+++ src/main/java/com/dev/planmytrip/controller/FlightController.java
@@ -3,6 +3,7 @@ package com.dev.planmytrip.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,12 @@ public class FlightController {
     public ResponseEntity<?> getFlight() {
 
         return new ResponseEntity<>("Flights are under maintenance", HttpStatus.OK);
+    }
+
+    @PostMapping("/filght")
+    public ResponseEntity<?> saveFlight() {
+
+        return new ResponseEntity<>("Flights are under maintenance", HttpStatus.ACCEPTED);
     }
 
 
